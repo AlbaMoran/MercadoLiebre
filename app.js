62d89d489393d367726bf78a0b9452621bc4e6ce
AlbaMoran <alba.morann@gmail.com>
2022-04-05 22:58:26 -0300

    se creo procfile e heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname,'public')));
 
-app.listen(3000, ()=> console.log('Levantando servidor en puerto 3000'));
+app.listen(process.env.PORT || 3000, ()=> console.log('Levantando servidor en puerto 3000'));
 
 app.get('/', function(req,res) {
     res.sendFile(path.resolve(__dirname, 'views/index.html'))
